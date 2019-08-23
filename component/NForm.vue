b39e1864068932fd753d471b8f6593c0e7420423
kietnt.pyn <kietnt.pyn@vnpt.vn>
2019-08-23 09:58:54 +0700

    k

--- component/NForm.vue
+++ component/NForm.vue
@@ -5,53 +5,47 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Emit, Model } from "vue-property-decorator";
-import isEmpty from "lodash/isEmpty";
+import { Component, Vue, Prop, Emit, Model } from 'vue-property-decorator'
+import isEmpty from 'lodash/isEmpty'
 @Component({})
 export default class NForm extends Vue {
-  @Prop({ type: Boolean, default: false }) lazyValidation!: boolean;
-  @Model("input", { type: Boolean, default: false }) value!: boolean;
+  @Prop({ type: Boolean, default: false }) lazyValidation!: boolean
+  @Model('input', { type: Boolean, default: false }) value!: boolean
   @Emit() input(e) {}
   @Emit() submit(e) {}
 
-  vueComponents: any[] = [];
+  vueComponents: any[] = []
 
   mounted() {
-    this.findDeep(this.$slots.default);
+    this.findDeep(this.$slots.default)
   }
 
   validate() {
-    const valid = !this.vueComponents
-      .map(v => v.validate(v.value))
-      .some(v => !v);
-    this.input(valid);
-    return valid;
+    const valid = !this.vueComponents.map(v => v.validate(v.value)).some(v => !v)
+    this.input(valid)
+    return valid
   }
 
   resetValidation() {
-    this.vueComponents.forEach(v => (v.valid = true));
-    this.input(false);
+    this.vueComponents.forEach(v => (v.valid = true))
+    this.input(false)
   }
 
   findDeep(obj) {
     if (obj instanceof Array) {
-      for (var i = 0; i < obj.length; i++) {
-        this.findDeep(obj[i]);
+      for (let i = 0; i < obj.length; i++) {
+        this.findDeep(obj[i])
       }
     } else {
-      if (!isEmpty(obj["children"])) {
-        this.findDeep(obj["children"]);
+      if (!isEmpty(obj['children'])) {
+        this.findDeep(obj['children'])
       }
-      if (
-        obj["componentInstance"] instanceof Vue &&
-        obj["componentInstance"].form
-      ) {
-        this.vueComponents.push(obj["componentInstance"]);
+      if (obj['componentInstance'] instanceof Vue && obj['componentInstance'].form) {
+        this.vueComponents.push(obj['componentInstance'])
       }
     }
   }
 }
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- component/NSelect2.vue
+++ component/NSelect2.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="{'form-group': form, 'has-error': !valid}">
+  <div :class="{ 'form-group': form, 'has-error': !valid }">
     <label v-if="hasLabel" class="control-label">{{ label }}</label>
     <select :class="cCssClass" :value="value"></select>
     <span v-if="!valid" class="help-block">{{ errorText }}</span>
@@ -7,54 +7,53 @@
   <!-- <select :class="selectCssClass"></select> -->
 </template>
 
-
 <script lang="ts">
-import { Component, Vue, Prop, Emit, Model } from "vue-property-decorator";
-import isEmpty from "lodash/isEmpty";
+import { Component, Vue, Prop, Model } from 'vue-property-decorator'
+import isEmpty from 'lodash/isEmpty'
 @Component({ inheritAttrs: false })
 export default class NSelect2 extends Vue {
-  @Prop({ type: String, default: "" }) cssClass!: string;
-  @Prop({ type: Boolean, default: false }) clearable!: boolean;
-  @Prop({ type: Boolean, default: false }) disabled!: boolean;
-  @Prop({ type: Boolean, default: false }) multiple!: boolean;
-  @Prop({ type: Boolean, default: false }) searchable!: boolean;
-  @Prop({ type: String, default: "text" }) itemText!: string;
-  @Prop({ type: String, default: "value" }) itemValue!: string;
-  @Prop({ type: Boolean, default: false }) form!: boolean;
-  @Prop(Array) items!: any[];
-  @Prop(String) hint!: string;
-  @Prop(String) label!: string;
-  @Prop(Array) rules!: any[];
-  @Model("input", [String, Number, Array, Object]) value!: any[] | any;
+  @Prop({ type: String, default: '' }) cssClass!: string
+  @Prop({ type: Boolean, default: false }) clearable!: boolean
+  @Prop({ type: Boolean, default: false }) disabled!: boolean
+  @Prop({ type: Boolean, default: false }) multiple!: boolean
+  @Prop({ type: Boolean, default: false }) searchable!: boolean
+  @Prop({ type: String, default: 'text' }) itemText!: string
+  @Prop({ type: String, default: 'value' }) itemValue!: string
+  @Prop({ type: Boolean, default: false }) form!: boolean
+  @Prop(Array) items!: any[]
+  @Prop(String) hint!: string
+  @Prop(String) label!: string
+  @Prop(Array) rules!: any[]
+  @Model('input', [String, Number, Array, Object]) value!: any[] | any
   input(e) {
-    this.validate(e);
-    this.$emit("input", isNaN(e) ? e : Number(e));
+    this.validate(e)
+    this.$emit('input', isNaN(e) ? e : Number(e))
   }
 
-  valid: boolean = true;
+  valid: boolean = true
   get hasLabel() {
-    return !isEmpty(this.label);
+    return !isEmpty(this.label)
   }
   get cCssClass() {
-    let css = "form-control ";
-    css += this.cssClass;
-    return css;
+    let css = 'form-control '
+    css += this.cssClass
+    return css
   }
   get errorText() {
     if (this.rules) {
-      let f = this.rules.find(r => r(this.value) !== true);
-      return f ? f(this.value) : "";
+      const f = this.rules.find(r => r(this.value) !== true)
+      return f ? f(this.value) : ''
     }
-    return "";
+    return ''
   }
   get select2Data() {
     return this.items.map(item => {
-      return { id: item[this.itemValue], text: item[this.itemText] };
-    });
+      return { id: item[this.itemValue], text: item[this.itemText] }
+    })
   }
 
   mounted() {
-    ($(this.$el).find("select") as any)
+    ;($(this.$el).find('select') as any)
       .select2({
         data: this.select2Data,
         allowClear: this.clearable,
@@ -65,16 +64,15 @@ export default class NSelect2 extends Vue {
       })
       .change(e => this.input(e.target.value))
       .val(this.value)
-      .trigger("change");
+      .trigger('change')
   }
 
   validate(value) {
-    this.valid = true;
-    if (this.rules) this.valid = !this.rules.some(e => e(value) !== true);
-    return this.valid;
+    this.valid = true
+    if (this.rules) this.valid = !this.rules.some(e => e(value) !== true)
+    return this.valid
   }
 }
 </script>
 
-<style>
-</style>
+<style></style>
