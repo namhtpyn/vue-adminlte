f6993ed1d18cabc811a30aa41c91f72c897a33b4
kietnt.pyn <kietnt.pyn@vnpt.vn>
2019-09-09 15:26:20 +0700

    fix search và focus on open dropdowntree tạo radio group button component tạo tab collection component

--- components/NDropDownTree.vue
+++ components/NDropDownTree.vue
@@ -1,17 +1,27 @@
 <template>
   <div :class="{ 'form-group': form, 'has-error': !valid }">
     <label v-if="hasLabel" class="control-label">{{ label }}</label>
-    <n-drop-down-list :value="value" @input="input" :text.sync="getText" :drop-down-width="dropDownWidth" @open="onOpen">
+    <n-drop-down-list
+      :value="value"
+      @input="input"
+      :text.sync="getText"
+      :drop-down-width="dropDownWidth"
+      @open="onOpen"
+      @close="onClose"
+    >
       <template #content="{data}">
         <n-tree
           :items="treeItems"
           :item-value="itemValue"
           :item-text="itemText"
-          :parent-key="parentKey"
+          :parent-key="treeParentKey"
           :expand-all="treeExpandAll"
           :expand-to-level="treeExpandToLevel"
           :icon="treeNodeIcon"
           :searchable="searchable"
+          :value="value"
+          fixed-search
+          height="260px"
           @select="item => itemSelect(item, data)"
         ></n-tree>
       </template>
@@ -39,7 +49,7 @@ export default class NDropDownTree extends Vue {
   @Prop({ type: Number, default: 0 }) treeExpandToLevel!: number
   @Prop({ type: String, default: 'value' }) itemValue!: string
   @Prop({ type: String, default: 'text' }) itemText!: string
-  @Prop({ type: String, default: 'parentID' }) parentKey!: string
+  @Prop({ type: [String, Number], default: 'parentID' }) treeParentKey!: string | number
 
   @Prop([String, Number]) dropDownWidth!: string | number
   @Prop() label!: string
@@ -52,7 +62,6 @@ export default class NDropDownTree extends Vue {
   get hasLabel() {
     return !_.isEmpty(this.label)
   }
-  //search=
   get getText() {
     if (!this.treeItems || this.treeItems.length <= 0) return ''
     const item = this.treeItems.find(item => item[this.itemValue] === this.value)
@@ -77,8 +86,18 @@ export default class NDropDownTree extends Vue {
     if (!this.lazyValidation || !this.valid) this.validate(item.id)
     data.isOpen = false
   }
+  treeComponent!: any
+  mounted() {
+    this.treeComponent = this.$children[0].$children[0] as any
+  }
   onOpen() {
-    //if (this.searchable) this.$nextTick(() => this.$children[0].$children[0].$el.querySelector('input').focus())
+    this.$nextTick(() => {
+      this.treeComponent.focusSelectedNode()
+      this.treeComponent.focusSearch()
+    })
+  }
+  onClose() {
+    this.treeComponent.searchText = ''
   }
 }
 </script>
