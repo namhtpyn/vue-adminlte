f1ff4a549193248a4046487dfe6de622bfd3d786
Nam Hoang <namht.pyn@vnpt.vn>
2019-09-11 14:00:54 +0700

    Fix component kebabcase error

--- demo/main.ts
+++ demo/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import kebabCase from 'lodash/kebabCase'
+import _ from 'lodash'
 if (process.env.NODE_ENV === 'development') {
   // Inject base component
   const baseComponent = require.context('../components/', true, /\.vue$/) // extract js files inside modules folder
@@ -8,8 +8,8 @@ if (process.env.NODE_ENV === 'development') {
     const pp = filePath.split('/')
     let fileName = pp[pp.length - 1].replace(/(\.\/|\.vue)/g, '')
     if (fileName === 'index') fileName = pp[pp.length - 2]
-    const moduleName = kebabCase(fileName) //
-    Vue.component(moduleName, baseComponent(filePath).default)
+    //const moduleName = _.kebabCase(fileName) //
+    Vue.component(fileName, baseComponent(filePath).default)
   })
   new Vue({
     el: '#app',
